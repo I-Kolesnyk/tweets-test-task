98581582fbdc641152567797c28e32978163f4ba
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-04-24 13:03:16 +0300

    add 404 page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -12,9 +12,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <HashRouter>
+      <BrowserRouter basename="/tweets-test-task">
           <App />
-        </HashRouter>
+          </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
